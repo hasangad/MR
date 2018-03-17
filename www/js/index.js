c1edc666af3976984ee17fdfe7615555367e9bc7
muhammadalaa <eng.muhammad_alaa@yahoo.com>
2018-03-17 14:48:43 +0300

    check push

---
+++ www/js/index.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        checkLanguage();
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
+
+
+    },
+    setupPush: function () {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+				senderID: "1067921236443"
+			},
+            "browser": {
+            },
+            "ios": {
+				"fcmSandbox":false,
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
+
+        push.on('registration', function (data) {
+            console.log('registration event: ' + data.registrationId);
+            //loginusername.val(data.registrationId);
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+
+            //var parentElement = document.getElementById('registration');
+            //var listeningElement = parentElement.querySelector('.waiting');
+            //var receivedElement = parentElement.querySelector('.received');
+
+            //listeningElement.setAttribute('style', 'display:none;');
+            //receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function (e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function (data) {
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+        });
+    }
+};
+
+	
+	/*var push = PushNotification.init({
+    android: {
+        senderID: "1067921236443"
+    },
+    ios: {
+        senderID: "1067921236443",
+        gcmSandbox: true,
+        alert: "true",
+        badge: true,
+        sound: 'true'
+    },
+    windows: {}
+});
+	
+	
+function exitFromApp()
+             {
+				 
+				 //alert("test");
+                navigator.app.exitApp();
+             }*/
+			 
+			 // Wait for PhoneGap to load
+/*document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+	
+    //alert("deviceready");
+    document.getElementsByClassName('exit_app').addEventListener('click', function() {
+		
+		//alert("test");
+        navigator.app.exitApp();
+    });
+}*/
+	
+	
+			 // Wait for PhoneGap to load
+//document.addEventListener("deviceready", onDeviceReady, false);
+			  //alert("Document is ready");
+
+function onDeviceReady() {
+	
+    //alert("deviceready");
+	$(".exit_app").click(function() {
+    //document.getElementsByClassName('exit_app').addEventListener('click'
+		
+		//alert("Test Exit App");
+        navigator.app.exitApp();
+    });
+}
+			
+			
+onDeviceReady();
+
+
+
+});
